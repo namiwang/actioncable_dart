f1cbe508ee0b0bfad49bb7a0810c2e2e6edc3b1a
Clinton Mbah <clintonmbah44@gmail.com>
2020-02-27 00:38:07 +0000

    added tests for channel_id

--- lib/channel_id.dart
+++ lib/channel_id.dart
@@ -1,18 +1,19 @@
-// 
+//
 // ActionCable's channel identifier is a bit annoying: it's a string of encoded JSON.
-// 
+//
 // Thus, we have to make sure we always provide same encoded String (as key when storing callback functions) from different inputs
 // - "{\"channel\": \"ChatChannel\", id: \"1\"}"
 // - "{id: \"1\", \"channel\": \"ChatChannel\"}"    // different order
 // - "{\"channel\":    \"ChatChannel\", id: \"1\"}" // additional spaces
-// 
+//
 // Currently, we achieve that by encoding params Map into a SplayTreeMap before stringifying.
 
 import 'dart:collection';
 import 'dart:convert';
 
 String encodeChannelId(String channelName, Map channelParams) {
-  final fullChannelName = channelName.endsWith('Channel') ? channelName : "${channelName}Channel";
+  final fullChannelName =
+      channelName.endsWith('Channel') ? channelName : "${channelName}Channel";
 
   Map channelId = channelParams == null ? {} : Map.from(channelParams);
   channelId['channel'] ??= fullChannelName;
@@ -21,8 +22,6 @@ String encodeChannelId(String channelName, Map channelParams) {
   return jsonEncode(orderedMap);
 }
 
-String parseChannelId(String receivedChannelId) {
-  return jsonEncode(
-    SplayTreeMap.from(jsonDecode(receivedChannelId))
-  );
+String parseChannelId(String channelId) {
+  return jsonEncode(SplayTreeMap.from(jsonDecode(channelId)));
 }
